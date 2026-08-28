48dacdc13fcadc2fb7acd3d98a0baeb4a5926dbf
Hugo Svirak <hugo@hugosvirak.com>
2026-08-29 00:37:43 +0200

    Add location page

---
+++ browser-client/src/routes/[locale]/location/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type { Locale } from "$lib/Locale";
+  import Footer from "../../../Footer.svelte";
+  import Header from "../../../Header.svelte";
+
+  let { locale }: { locale: Locale } = $props()
+
+</script>
+
+<Header locale={locale} />
+<section>
+  <div class="section-inner">
+      <h1>Select Location</h1>
+      <button>Hugo's Tech Lab</button>
+  </div>
+</section>
+<Footer />
+<style>
+  section {
+    min-height: 80vh;
+  }
+
+  .section-inner {
+        border: 1px solid rgb(0, 36, 156);
+  }
+</style>
